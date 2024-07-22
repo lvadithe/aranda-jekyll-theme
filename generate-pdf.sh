f247b90389418e3d4984e8f7a40b35eadc33feb8
Vadith Gomez Triviño <vadith.gomez@arandaSoft.com>
2024-07-22 16:19:31 -0500

    ajustes generador de pdf

--- generate-pdf.sh
+++ generate-pdf.sh
@@ -1,7 +1,7 @@
 # Remove existing pdf.md to create new file
 rm _pages/pdf.md
 
-# List all files ending with .md in the _pages directory, sorting by numerical order starting from second field separated by "/"
+# list all files ending with .md in the _page directory sorting by numerical order starting from second field separated by "/"
 FILES=$(find _pages -type f -name "*.md" | sort -t '/' -k 2n)
 
 # Getting manual name from index.md
@@ -18,10 +18,10 @@ layout: pdf
 for line in $FILES
 do
     # Getting chapter name to write as a title inside pdf.md. Search the line that contains "excerpt: " and parse text to get only name
-    CHAPTER=$(grep "excerpt: " "$line" | cut -d ":" -f2 | tr -d '"' | cut -d " " -f2-)
+    CHAPTER=$(grep "excerpt: " $line | cut -d ":" -f2 | tr -d '"' | cut -d " " -f2-)
 
-    # Check if this chapter exists in pdf.md to avoid repeated names in case the chapter markdown contains this name
-    EXISTS_CHAPTER=$(grep "$CHAPTER" _pages/pdf.md | wc -l)
+    # Check if this chapter  exists in pdf.md to avoid repeated names in case the chapter markdown contains this name
+    EXISTS_CHAPTER=$(grep "$CHAPTER" _pages/pdf.md | wc -w)
 
     # Skip the main markdown. The Introduction chapter and Main contain the same prologue.
     if [[ $CHAPTER != "Inicio" ]]
@@ -29,24 +29,25 @@ do
         # Write chapter if not exists
         if [ $EXISTS_CHAPTER -eq 0 ]
         then
-            echo -e "\n# $CHAPTER" >> _pages/pdf.md
+            echo $CHAPTER
+            echo "\n# $CHAPTER" >> _pages/pdf.md
         fi
     fi
 
     # Getting manual title to write inside pdf.md. Search the line that contains "title: " and parse text to get only name
-    TITLE=$(grep "title: " "$line" | cut -d ":" -f2 | tr -d '"' | cut -d " " -f2-)
-    # Check if manual title exists
-    EXISTS_TITLE=$(grep "$TITLE" _pages/pdf.md | wc -l)
+    TITLE=$(grep "title: " $line | cut -d ":" -f2 | tr -d '"' | cut -d " " -f2-)
+    # Check if manual title exiss
+    EXISTS_TITLE=$(grep "$TITLE" _pages/pdf.md | wc -w)
 
     if [ $EXISTS_TITLE -eq 0 ]
     then
-        echo -e "\n## $TITLE" >> _pages/pdf.md
+        echo "\n## $TITLE" >> _pages/pdf.md
     fi
 
-    # Extract the PDF file path from the .md file and adjust the name
-    PDF_PATH=$(grep "pdf: " "$line" | cut -d ":" -f2 | tr -d '"' | cut -d " " -f2-)
-    PDF_NAME=$(basename "$(dirname "$PDF_PATH")").pdf
+    # Find the line where the comments end
+    FROM=$(grep -n "\b---\b" $line | tail -1 | cut -d ":" -f1)
+    FROM=$(($FROM + 1))
 
-    # Append the PDF name to pdf.md
-    echo -e "\n### $PDF_NAME\n" >> _pages/pdf.md
+    # Write inside pdf.md from below line number to eof
+    tail -n +$FROM $line >> _pages/pdf.md
 done
