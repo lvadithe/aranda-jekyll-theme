a1bd7af5fdf75fc0dca298147ed1e0d8034b5fa4
Vadith Gomez Triviño <vadith.gomez@arandaSoft.com>
2024-07-23 11:27:25 -0500

    ajustes generador de pdf

--- generate-pdf.sh
+++ generate-pdf.sh
@@ -1,61 +1,58 @@
 #!/bin/bash
 
-# Remove existing pdf.md to create new file
+# Remove existing pdf.md to create a new file
 rm _pages/pdf.md
 
-# list all files in the _page directory sorting by numerical order starting from second field separated by "/"
+# List all files in the _pages directory, sorting by numerical order starting from the second field separated by "/"
 FILES=$(find _pages -type f | sort -t '/' -k 2n)
 
 # Getting manual name from index.md
 MANUALNAME=$(grep 'title' _pages/index.md)
 
-# Create and initialize pdf.md to create page with complete information
+# Create and initialize pdf.md to create a page with complete information
 echo "---
 $MANUALNAME
 permalink: /pdf
 layout: pdf
 ---" > _pages/pdf.md
 
-# Running through list of files $FILES to then open it and concat with pdf.md
+# Running through the list of files $FILES to process each one
 for line in $FILES
 do
-    # Adjust paths and write inside pdf.md from below line number to eof
-    sed -i 's/\/en\///g' "$line" # Remove /en/ prefix in the file itself
+    # Adjust paths and write inside pdf.md from below line number to EOF
+    sed -i 's/\/en\/assets\//\/assets\//g; s/\/en\//\//g' "$line"
 
-    # Adjust paths and write inside pdf.md from below line number to eof
-    sed 's/\/en\///g' "$line" | {
-        # Getting chapter name to write as a title inside pdf.md. Search the line that contains "excerpt: " and parse text to get only name
-        CHAPTER=$(grep "excerpt: " "$line" | cut -d ":" -f2- | tr -d '"' | cut -d " " -f2-)
+    # Getting chapter name to write as a title inside pdf.md.
+    CHAPTER=$(grep "excerpt: " "$line" | cut -d ":" -f2- | tr -d '"' | cut -d " " -f2-)
 
-        # Check if this chapter exists in pdf.md to avoid repeated names in case the chapter markdown contains this name
-        EXISTS_CHAPTER=$(grep "$CHAPTER" _pages/pdf.md | wc -l)
+    # Check if this chapter exists in pdf.md to avoid repeated names.
+    EXISTS_CHAPTER=$(grep "$CHAPTER" _pages/pdf.md | wc -l)
 
-        # Skip the main markdown. The Introduction chapter and Main contain the same prologue.
-        if [[ "$CHAPTER" != "Inicio" ]]
+    # Skip the main markdown. The Introduction chapter and Main contain the same prologue.
+    if [[ "$CHAPTER" != "Inicio" ]]
+    then
+        # Write chapter if not exists
+        if [ $EXISTS_CHAPTER -eq 0 ]
         then
-            # Write chapter if not exists
-            if [ $EXISTS_CHAPTER -eq 0 ]
-            then
-                echo $CHAPTER
-                echo "\n# $CHAPTER" >> _pages/pdf.md
-            fi
+            echo $CHAPTER
+            echo "\n# $CHAPTER" >> _pages/pdf.md
         fi
+    fi
 
-        # Getting manual title to write inside pdf.md. Search the line that contains "title: " and parse text to get only name
-        TITLE=$(grep "title: " "$line" | cut -d ":" -f2- | tr -d '"' | cut -d " " -f2-)
-        # Check if manual title exists
-        EXISTS_TITLE=$(grep "$TITLE" _pages/pdf.md | wc -l)
+    # Getting manual title to write inside pdf.md.
+    TITLE=$(grep "title: " "$line" | cut -d ":" -f2- | tr -d '"' | cut -d " " -f2-)
+    # Check if manual title exists
+    EXISTS_TITLE=$(grep "$TITLE" _pages/pdf.md | wc -l)
 
-        if [ $EXISTS_TITLE -eq 0 ]
-        then
-            echo "\n## $TITLE" >> _pages/pdf.md
-        fi
+    if [ $EXISTS_TITLE -eq 0 ]
+    then
+        echo "\n## $TITLE" >> _pages/pdf.md
+    fi
 
-        # Find the line where the comments end
-        FROM=$(grep -n "\b---\b" "$line" | tail -1 | cut -d ":" -f1)
-        FROM=$(($FROM + 1))
+    # Find the line where the content begins after the YAML front matter
+    FROM=$(grep -n "\b---\b" "$line" | tail -1 | cut -d ":" -f1)
+    FROM=$(($FROM + 1))
 
-        # Write inside pdf.md from below line number to eof
-        tail -n +$FROM "$line" >> _pages/pdf.md
-    }
+    # Write inside pdf.md from below line number to EOF
+    tail -n +$FROM "$line" >> _pages/pdf.md
 done
